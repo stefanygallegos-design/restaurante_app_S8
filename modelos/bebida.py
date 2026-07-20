af2d2140b1410bdda0d0449fc6b399ac12638ed6
Stefany Gallegos <stefanyzari@gmail.com>
2026-07-19 20:02:42 -0500

    Update bebida.py

--- modelos/bebida.py
+++ modelos/bebida.py
@@ -1,1 +1,23 @@
+from modelos.producto import Producto
 
+
+class Bebida(Producto):
+    def __init__(
+        self,
+        codigo: str,
+        nombre: str,
+        categoria: str,
+        precio: float,
+        tamano: str,
+        envase: str,
+    ):
+        super().__init__(codigo, nombre, categoria, precio)
+        self.tamano = tamano
+        self.envase = envase
+
+    def mostrar_informacion(self) -> str:
+        return (
+            super().mostrar_informacion()
+            + f"\nTamaño: {self.tamano}"
+            + f"\nEnvase: {self.envase}"
+        )
