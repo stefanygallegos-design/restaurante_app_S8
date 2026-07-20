426ce9f0ef871cb7e8c9d829a82bc48a51beaa97
Stefany Gallegos <stefanyzari@gmail.com>
2026-07-19 20:02:19 -0500

    Update producto.py

--- modelos/producto.py
+++ modelos/producto.py
@@ -1,1 +1,14 @@
+class Producto:
+    def __init__(self, codigo: str, nombre: str, categoria: str, precio: float):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
 
+    def mostrar_informacion(self) -> str:
+        return (
+            f"Código: {self.codigo}\n"
+            f"Nombre: {self.nombre}\n"
+            f"Categoría: {self.categoria}\n"
+            f"Precio: ${self.precio:.2f}"
+        )
