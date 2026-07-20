535351d23d58be5de4d216c31304079a918d468a
Stefany Gallegos <stefanyzari@gmail.com>
2026-07-19 20:03:44 -0500

    Update cliente.py

--- modelos/cliente.py
+++ modelos/cliente.py
@@ -1,1 +1,12 @@
+class Cliente:
+    def __init__(self, identificacion: str, nombre: str, correo: str):
+        self.identificacion = identificacion
+        self.nombre = nombre
+        self.correo = correo
 
+    def mostrar_informacion(self) -> str:
+        return (
+            f"Identificación: {self.identificacion}\n"
+            f"Nombre: {self.nombre}\n"
+            f"Correo: {self.correo}"
+        )
