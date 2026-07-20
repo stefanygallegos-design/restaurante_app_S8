f69e87f507d544aa602702bbe37ce65da79f5f36
Stefany Gallegos <stefanyzari@gmail.com>
2026-07-19 20:04:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,90 @@
+from modelos.producto import Producto
+from modelos.bebida import Bebida
+from modelos.cliente import Cliente
+from servicios.restaurante import Restaurante
 
+
+def menu():
+    print("\n====================================")
+    print("      SISTEMA DE RESTAURANTE")
+    print("====================================")
+    print("1. Registrar producto")
+    print("2. Registrar bebida")
+    print("3. Registrar cliente")
+    print("------------------------------------")
+    print("4. Listar productos")
+    print("5. Listar clientes")
+    print("------------------------------------")
+    print("6. Salir")
+
+
+def main():
+
+    restaurante = Restaurante()
+
+    while True:
+
+        menu()
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+
+            codigo = input("Código: ")
+            nombre = input("Nombre: ")
+            categoria = input("Categoría: ")
+            precio = float(input("Precio: "))
+
+            producto = Producto(codigo, nombre, categoria, precio)
+            restaurante.registrar_producto(producto)
+
+        elif opcion == "2":
+
+            codigo = input("Código: ")
+            nombre = input("Nombre: ")
+            categoria = input("Categoría: ")
+            precio = float(input("Precio: "))
+            tamano = input("Tamaño: ")
+            envase = input("Tipo de envase: ")
+
+            bebida = Bebida(
+                codigo,
+                nombre,
+                categoria,
+                precio,
+                tamano,
+                envase,
+            )
+
+            restaurante.registrar_producto(bebida)
+
+        elif opcion == "3":
+
+            identificacion = input("Identificación: ")
+            nombre = input("Nombre: ")
+            correo = input("Correo: ")
+
+            cliente = Cliente(
+                identificacion,
+                nombre,
+                correo,
+            )
+
+            restaurante.registrar_cliente(cliente)
+
+        elif opcion == "4":
+            restaurante.listar_productos()
+
+        elif opcion == "5":
+            restaurante.listar_clientes()
+
+        elif opcion == "6":
+            print("Gracias por utilizar el sistema.")
+            break
+
+        else:
+            print("Opción inválida.")
+
+
+if __name__ == "__main__":
+    main()
